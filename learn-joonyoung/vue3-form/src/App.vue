b3e79e6423913d9f2a3219db5529c29dab46d418
Joonyoung <aimzero9303@gmail.com>
2023-10-22 16:12:31 +0900

    로그인 폼 구성 및 폼 이벤트 제어, 서버로 데이터 전송

---
+++ learn-joonyoung/vue3-form/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <form action="" @submit.prevent="submitForm">
+        <div>
+            <label for="userId">ID:</label>
+            <input id="userId" type="text" v-model="username" />
+        </div>
+        <div>
+            <label for="password">PW:</label>
+            <input id="password" type="password" v-model="password" />
+        </div>
+        <button type="submit">로그인</button>
+    </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            username: "",
+            password: "",
+        };
+    },
+    methods: {
+        submitForm() {
+            // preventDefault를 메소드에서 직접 하지 않고 form 태그에서 바로 @submit.prevent로도 할 수 있다.
+            // event.preventDefault();
+            axios
+                .post("https://jsonplaceholder.typicode.com/users/", {
+                    username: this.username,
+                    password: this.password,
+                })
+                .then((res) => {
+                    console.log(res);
+                });
+            console.log("제출됨");
+        },
+    },
+};
+</script>
+
+<style scoped></style>
