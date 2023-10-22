d1a39045acead74b6e5b0a6e6f6f58aecd76d33c
Joonyoung <aimzero9303@gmail.com>
2023-10-22 14:36:48 +0900

    싱글 파일 컴포넌트의 props, event emit

---
+++ learn-joonyoung/vue3-cli/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <AppHeader v-bind:title="msg" v-on:change="changeMessage" />
+    <div>
+        {{ msg }}
+    </div>
+    <button @click="showAlert">경고</button>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+    components: {
+        AppHeader,
+    },
+    data() {
+        return {
+            msg: "앱 헤더 컴포넌트",
+        };
+    },
+    methods: {
+        showAlert() {
+            alert("hello");
+        },
+        changeMessage() {
+            this.msg = "메세지 변경됨!";
+        },
+    },
+};
+</script>
+
+<style scoped></style>
---
+++ learn-joonyoung/vue3-cli/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ title }}</h1>
+    <button @click="changeTitle">타이틀 변경</button>
+</template>
+
+<script>
+export default {
+    props: ["title"],
+    methods: {
+        changeTitle() {
+            this.$emit("change");
+        },
+    },
+};
+</script>
+
+<style scoped></style>
